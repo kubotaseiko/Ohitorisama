6165d3b32d05513c83e093eec5514b7560d23f50
kubotaseiko <kubota712.050@gmail.com>
2021-06-14 05:20:53 +0000

    [add]adminページ

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -1,2 +1,7 @@
 class Admin::ContactsController < ApplicationController
+
+  def index
+    @contacts = Contact.all.page(params[:page]).per(10)
+  end
+
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,15 @@
 class Admin::UsersController < ApplicationController
+
+  def index
+    @users = User.all.page(params[:page]).per(10)
+  end
+
+  def edit
+    @users = User.find(params[:id])
+  end
+
+  def update
+    @users = User.find(params[:id])
+  end
+
 end
