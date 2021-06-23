9f8b5927a47c6bca93f568bcbfe768bc0bd782fe
kubotaseiko <kubota712.050@gmail.com>
2021-06-23 10:29:43 +0000

    レイアウト調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
 
   has_many :shops, dependent: :destroy
   has_many :tweets, dependent: :destroy
